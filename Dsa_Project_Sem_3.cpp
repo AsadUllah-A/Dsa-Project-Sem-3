c1da227fa0e23227321911f3e524dbc666e24a0f
akswati <asadullahswati55@gmail.com>
2024-11-15 12:02:48 +0500

    Main

---
+++ Dsa_Project_Sem_3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+    cout << "\n\n\t**** Event Ticket Reservation System ****\n";
+    cout << "\n\t\t ______________________________________________\n";
+    cout << "\t\t|       |" << setw(40) << "|\n";
+    cout << "\t\t| [1]   |     Sign Up" << setw(28) << "|\n";
+    cout << "\t\t| [2]   |     Login" << setw(30) << "|\n";
+    cout << "\t\t| [3]   |     Admin Panel" << setw(24) << "|\n";
+    cout << "\t\t| [4]   |     Search Events" << setw(22) << "|\n";
+    cout << "\t\t| [5]   |     Exit" << setw(31) << "|\n";
+    cout << "\t\t||_|\n";
+
+    return 0;
+}
